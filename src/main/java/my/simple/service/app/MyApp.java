00d73e64626ba64dc476f0de415a86b0ee56d280
t3rmin41 <t33rmin41@gmail.com>
2016-03-14 20:49:22 +0200

    Added logback logging. Web-Service is at http://localhost:9999/ws/countries.wsdl

--- src/main/java/my/simple/service/app/MyApp.java
+++ src/main/java/my/simple/service/app/MyApp.java
@@ -2,6 +2,8 @@ package my.simple.service.app;
 
 import my.simple.service.config.MyWebServiceConfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,8 +13,10 @@ import org.springframework.context.annotation.Import;
 @Import({MyWebServiceConfig.class})
 public class MyApp {
 
+    private static Logger log = LoggerFactory.getLogger(MyApp.class);
+    
     public static void main(String args[]) {
         ApplicationContext context = SpringApplication.run(MyApp.class, args);
-        System.out.println("My countries web service has started, context ID : " + context.getId());
+        log.info("My countries web service has started, context ID : " + context.getId());
     }
 }
